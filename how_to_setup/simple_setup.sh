130f21617d6c37c6230184344a714d8237a188c4
HoangYell <hoang.ngo@paradox.ai>
2022-05-29 00:49:32 +0700

    docs(5): update README.md

---
+++ how_to_setup/simple_setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# git clone & activate virtual env
+git clone git@github.com:ngohoangyell/Hoang_Yell_Backend_Engineer_Technical_Assessment.git
+cd Hoang_Yell_Backend_Engineer_Technical_Assessment
+python3 -m venv hybeta_env
+source hybeta_env/bin/activate
+# install python packages
+python3 -m pip install -r requirements.txt
+# install coding cleaner, helpful for coding, you can skip it
+pre-commit install
+pre-commit run --all-files
+# create database model
+python manage.py migrate
+# run pytest
+pytest-s
+# start server
+python manage.py runserver
+
+
+#==========================Some more info if you have time===============================
+# Create example data for list view
+# sh create_example_data.sh
+
+# Command to start django project from 0
+# django-admin startproject hybeta .
+# python manage.py startapp jobs
+# PYTHONUNBUFFERED=1;DJANGO_SETTINGS_MODULE=hybeta.settings.test python manage.py shell
+
+# Tips:
+# kill port: kill -9 $(lsof -ti:8000)
